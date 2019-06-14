ed83bd4f3e74820531aa785219479b0a052e6f48
chenjiegd <chenjiegd@foxmail.com>
2019-06-14 11:44:02 +0800

    资料保存

--- main.ts
+++ main.ts
@@ -5,3 +5,23 @@ basic.showLeds(`
     # . . . #
     . # # # .
     `);
+
+namespace microbit_test{
+    export enum enColor{
+        OFF = 0,
+        Red,
+        Green,
+        Blue,
+        White,
+        Cyan,
+        Pinkish,
+        Yellow
+	}
+	
+    export enum enLED1{
+        OFF = 0,
+        ON = 1
+	}
+	
+	
+}
